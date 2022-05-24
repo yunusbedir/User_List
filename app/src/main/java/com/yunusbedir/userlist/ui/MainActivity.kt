1305a10a7f6be547d40a87cddedaf07f3f7eab09
yunusbedir <yunusbedir@outlook.com.tr>
2022-05-24 23:29:44 +0300

    changed location of MainActivity.kt

--- app/src/main/java/com/yunusbedir/userlist/ui/MainActivity.kt
+++ app/src/main/java/com/yunusbedir/userlist/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yunusbedir.userlist
+package com.yunusbedir.userlist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yunusbedir.userlist.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
