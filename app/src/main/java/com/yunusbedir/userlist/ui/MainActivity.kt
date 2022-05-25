029c6505f4af44d3acf50c5e314df7184058d534
yunusbedir <yunusbedir@outlook.com.tr>
2022-05-25 14:56:56 +0300

    added navigation component and SwipeRefreshLayout

--- app/src/main/java/com/yunusbedir/userlist/ui/MainActivity.kt
+++ app/src/main/java/com/yunusbedir/userlist/ui/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.yunusbedir.userlist.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yunusbedir.userlist.R
+import androidx.appcompat.app.AppCompatActivity
+import com.yunusbedir.userlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/yunusbedir/userlist/ui/main/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.yunusbedir.userlist.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yunusbedir.userlist.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater)
+        return binding.root
+    }
+
+}
