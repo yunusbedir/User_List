951ab11f37284fffa2e7c77dc11a74c0d2fbf035
yunusbedir <yunusbedir@outlook.com.tr>
2022-05-26 13:53:37 +0300

    fetched user list was displayed on the screen

--- app/src/main/java/com/yunusbedir/userlist/ui/main/MainFragment.kt
+++ app/src/main/java/com/yunusbedir/userlist/ui/main/MainFragment.kt
@@ -1,18 +1,26 @@
 package com.yunusbedir.userlist.ui.main
 
+import com.yunusbedir.userlist.data.FetchResponse
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.yunusbedir.userlist.data.Person
 import com.yunusbedir.userlist.databinding.FragmentMainBinding
+import com.yunusbedir.userlist.domain.UiState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
 
     private val mainViewModel by viewModels<MainViewModel>()
+    private val userListAdapter = UserListAdapter()
 
     private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
@@ -21,6 +29,44 @@ class MainFragment : Fragment() {
     ): View? {
         binding = FragmentMainBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mainSwipeRefreshLayout.setOnRefreshListener {
+            userListAdapter.submitList(null)
+            mainViewModel.nextUserList()
+        }
+        binding.userListRecyclerView.adapter = userListAdapter
+        initObserver()
+    }
+
+    private fun initObserver() = lifecycleScope.launch {
+        mainViewModel.userListLiveData
+            .flowWithLifecycle(lifecycle, Lifecycle.State.STARTED)
+            .collect {
+                when (it) {
+                    is UiState.Success<*> -> {
+                        val userList = ArrayList<Person>()
+                        userList.addAll(userListAdapter.currentList)
+                        userList.addAll((it.data as FetchResponse).people)
+                        userListAdapter.submitList(
+                            userList
+                        )
+                        progressVisibility(false)
+                    }
+                    is UiState.Fail -> {
+                        progressVisibility(false)
+                    }
+                    UiState.Loading -> {
+                        progressVisibility(true)
+                    }
+                }
+            }
+    }
+
+    fun progressVisibility(isVisible: Boolean) {
+        binding.mainSwipeRefreshLayout.isRefreshing = isVisible
     }
 
 }
--- app/src/main/java/com/yunusbedir/userlist/ui/main/MainViewModel.kt
+++ app/src/main/java/com/yunusbedir/userlist/ui/main/MainViewModel.kt
@@ -1,11 +1,34 @@
 package com.yunusbedir.userlist.ui.main
 
-import DataSource
+import com.yunusbedir.userlist.data.FetchResponse
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yunusbedir.userlist.domain.UiState
+import com.yunusbedir.userlist.domain.usecase.FetchUserListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
+    private val fetchUserListUseCase: FetchUserListUseCase
 ) : ViewModel() {
+
+    private val _userListLiveData = MutableSharedFlow<UiState>()
+    val userListLiveData: SharedFlow<UiState> = _userListLiveData
+
+    private var nextUserList: Int = 0
+
+    fun nextUserList() {
+        viewModelScope.launch {
+            fetchUserListUseCase.invoke(nextUserList).collect {
+                if (it is UiState.Success<*>) {
+                    nextUserList = ((it.data as FetchResponse).next ?: "0").toInt()
+                }
+                _userListLiveData.emit(it)
+            }
+        }
+    }
 }
